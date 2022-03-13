2d1c5a6203ac8a5239f64dfd72c6310a27df54c0
SalmaIssa96 <salmaissa196@gmail.com>
2022-03-14 00:39:16 +0200

    finish  sgin up route relates #11

--- server/controllers/errorHandle/serverError.js
+++ server/controllers/errorHandle/serverError.js
@@ -2,7 +2,7 @@ const { join } = require('path');
 
 const serverError = (err, req, res, next) => {
   // res.status(500).json({ err, status: 500 });
-  const file = join(__dirname, '..', '..', '..', 'public', 'html', '500.html');
+  const file = join(__dirname, '..', '..', '..', 'public', 'static', 'html', '500.html');
   res.status('500').sendFile(file);
 };
 
