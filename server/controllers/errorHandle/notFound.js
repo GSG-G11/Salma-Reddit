c99a5003d483db74cceb3799aed5020c98b368f6
SalmaIssa96 <salmaissa196@gmail.com>
2022-03-14 01:53:04 +0200

    create login route with server side validation relates #13

--- server/controllers/errorHandle/notFound.js
+++ server/controllers/errorHandle/notFound.js
@@ -1,7 +1,7 @@
 const { join } = require('path');
 
 const notFoundError = (req, res) => {
-  const file = join(__dirname, '..', '..', '..', 'public', 'html', '404.html');
+  const file = join(__dirname, '..', '..', '..', 'public', 'static', 'html', '404.html');
   res.status('404').sendFile(file);
 };
 
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const signup = require('../controllers/users/signup');
+const { signup, login } = require('../controllers');
 
 const routerAPI = express.Router();
 routerAPI.post('/signup', signup);
-
+routerAPI.post('/login', login);
 module.exports = routerAPI;
